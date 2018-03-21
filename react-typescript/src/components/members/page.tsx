b3ae70f63a4262edd600a5175ad68106e2234bd0
e <1127431465@qq.com>
2018-03-21 23:53:29 +0800

    typescript语言,webpack配置tsx文件，react设置路由

---
+++ react-typescript/src/components/members/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export const MemebersPage: React.StatelessComponent<{}> = () => {
+    return (
+        <div className="row">
+            <h2>
+                Members Page
+            </h2>
+        </div>
+    )
+}
